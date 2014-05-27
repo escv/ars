0cc0be962280f2a840dda0cd43ee8f5dfe23ad88
Andre Albert <andre.albert82@googlemail.com>
2014-05-27 18:10:38 +0200

    further work, backup remote

--- ars-masterdata/ars-masterdata-service/src/main/java/com/prodyna/pac/ars/masterdata/AircraftServiceBean.java
+++ ars-masterdata/ars-masterdata-service/src/main/java/com/prodyna/pac/ars/masterdata/AircraftServiceBean.java
@@ -2,15 +2,19 @@ package com.prodyna.pac.ars.masterdata;
 
 import java.util.List;
 
+import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.prodyna.pac.ars.masterdata.model.Aircraft;
 
 @Stateless
+@Local(AircraftService.class)
 @Path("/aircraft")
 public class AircraftServiceBean implements AircraftService {
 
@@ -24,6 +28,7 @@ public class AircraftServiceBean implements AircraftService {
 	}
 
 	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public List<Aircraft> readAllAircrafts() {
 		return (List<Aircraft>) em.createQuery("select c from Aircraft c", Aircraft.class);
