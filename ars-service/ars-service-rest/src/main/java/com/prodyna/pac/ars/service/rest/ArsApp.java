61223d8fe25abd315c6ceb83fff9cbaa0dbcd5fe
Andre Albert <andre.albert@prodyna.com>
2014-05-31 10:23:10 +0200

    further work

---
+++ ars-service/ars-service-rest/src/main/java/com/prodyna/pac/ars/service/rest/ArsApp.java
@@ -0,0 +1,10 @@
+package com.prodyna.pac.ars.service.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ArsApp extends Application {
+
+}
+
